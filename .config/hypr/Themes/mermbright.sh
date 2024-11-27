c48ddc0afcfaa6cfe6b28a763a2bade20eb09fda
emmabori <collin00205@gmail.com>
2024-11-27 23:07:28 +0100

    Changes to be committed: 	new file:   .config/hypr/Themes/lain.sh 	new file:   .config/hypr/Themes/mermbright.sh 	new file:   .config/hypr/animations.conf 	new file:   .config/hypr/constant.conf 	new file:   .config/hypr/themes/lainland.conf 	new file:   .config/hypr/themes/mermbrightland.conf 	new file:   .config/hypr/themes/template.conf 	new file:   .config/waybar/themes/lain/colors.css 	new file:   .config/waybar/themes/lain/config 	new file:   .config/waybar/themes/lain/style.css

---
+++ .config/hypr/Themes/mermbright.sh
@@ -0,0 +1,7 @@
+cp /home/emmabori/.config/hypr/themes/mermbrightland.conf /home/emmabori/.config/hypr/hyprland.conf
+
+cp /home/emmabori/.config/waybar/themes/papi/config /home/emmabori/.config/waybar/config
+cp /home/emmabori/.config/waybar/themes/papi/style.css /home/emmabori/.config/waybar/style.css
+cp /home/emmabori/.config/waybar/themes/papi/colors.css /home/emmabori/.config/waybar/colors.css
+
+/home/emmabori/.config/hypr/Themes/scripts/waybarrestart.sh
