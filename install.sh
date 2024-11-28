12b925640f434b31c672a74d3f45b9cd726f3077
emmabori <98230212+emmabori@users.noreply.github.com>
2024-11-28 21:04:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-yay -S hyprland-git hyprsunset rofi alacritty thunar ttf-jetbrains-mono-nerd waybar waybar-updates swww hyprpolkitagent-git grimshot zenity vscodium
-mv ~/dots/.config/* ~/.config/
+yay -S --needed hyprland-git hyprsunset rofi alacritty thunar ttf-jetbrains-mono-nerd waybar waybar-updates swww hyprpolkitagent-git grimshot zenity vscodium wlogout
+mv --backup=numbered ~/dots/.config/* ~/.config/
